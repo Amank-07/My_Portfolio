49a81cb22d5bbc8604e24903c916cdc37dcc144e
Amank-07 <ui22cs05@iiitsurat.com>
2025-10-26 17:57:57 +0530

    Add new projects and GitHub icon: Interview AI, Document Search System, FitTracker, and GitHub social link

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,11 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+
+
+// resume preview function
+function openResumePreview() {
+  const resumeUrl = './Resumee_aman.pdf';
+  window.open(resumeUrl, '_blank');
+}
